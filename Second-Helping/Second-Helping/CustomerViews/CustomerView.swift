6e013724e51b7448dd54e56fdc9f86a0b4f0e508
ninabraddock <113557496+ninabraddock@users.noreply.github.com>
2024-09-27 18:09:40 -0400

    fixed transition from ContentView to SearchView/CustomerView

--- Second-Helping/Second-Helping/CustomerViews/CustomerView.swift
+++ Second-Helping/Second-Helping/CustomerViews/CustomerView.swift
@@ -14,9 +14,7 @@ struct CustomerView: View {
     var body: some View {
         VStack {
             HStack {
-                
                 Spacer()
-                
                 Button(action: {
                     isLoggedIn = false
                 }) {
@@ -30,17 +28,90 @@ struct CustomerView: View {
             Spacer()
             
             TabView {
-                SearchView(
-                    image: Image("waterworks"),
-                    quantity: 3,
-                    name: "Waterworks",
-                    bagType: "Mystery",
-                    rangePickUpTime: "5:30 pm - 6:00 pm",
-                    ranking: 4.3,
-                    distance: 0.3,
-                    price: 15.00,
-                    btnHandler: nil
-                )
+                let customGreen = Color(hex: "#4f7942")
+                VStack {
+                    Text("Browse food options")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(customGreen)
+                    // Section for Dinner
+                    HStack{
+                        Text("Dinner")
+                            .underline()
+                            .font(.title2)
+                            .padding()
+                        Spacer()
+                        NavigationLink(destination: StatsView()) {
+                            Text("See All")
+                                .padding()
+                        }
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            SearchView(
+                                image: Image("waterworks"),
+                                quantity: Int(3),
+                                name: "Waterworks",
+                                bagType: "Mystery Bag",
+                                rangePickUpTime: "5:30 pm - 6:00 pm",
+                                ranking: 4.3,
+                                distance: 0.3,
+                                price: 15.00,
+                                btnHandler: nil
+                            )
+                            SearchView(
+                                image: Image("PhoHong"),
+                                quantity: Int(2),
+                                name: "Pho Hong",
+                                bagType: "Mystery Bag",
+                                rangePickUpTime: "8:30 pm - 9:00 pm",
+                                ranking: 4.8,
+                                distance: 1.1,
+                                price: 4.50,
+                                btnHandler: nil
+                            )
+                        }
+                    }
+
+                    // Section for Lunch
+                    HStack{
+                        Text("Lunch")
+                            .underline()
+                            .font(.title2)
+                            .padding()
+                        Spacer()
+                        NavigationLink(destination: StatsView()) {
+                            Text("See All")
+                                .padding()
+                        }
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            SearchView(
+                                image: Image("theGriffin"),
+                                quantity: Int(4),
+                                name: "The Griffin",
+                                bagType: "Mystery Bag",
+                                rangePickUpTime: "1:00 pm - 1:45 pm",
+                                ranking: 4.4,
+                                distance: 0.8,
+                                price: 5.50,
+                                btnHandler: nil
+                            )
+                            SearchView(
+                                image: Image("blueBird"),
+                                quantity: Int(2),
+                                name: "BlueBird Barbeque",
+                                bagType: "Mystery Bag",
+                                rangePickUpTime: "12:15 pm - 1:15 pm",
+                                ranking: 3.2,
+                                distance: 0.6,
+                                price: 6.00,
+                                btnHandler: nil
+                            )
+                        }
+                    }
+                }
                     .tabItem() {
                         Image(systemName: "magnifyingglass")
                         Text("Search")
