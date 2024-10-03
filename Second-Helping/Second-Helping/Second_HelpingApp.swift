236a7e2be3d46fdea51a13f4d29203e59694b2e0
Charlie Corriero <112521858+Charlie-Corriero@users.noreply.github.com>
2024-10-03 10:36:42 -0400

    Added the Firebase initialization

--- Second-Helping/Second-Helping/Second_HelpingApp.swift
+++ Second-Helping/Second-Helping/Second_HelpingApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Second_HelpingApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
