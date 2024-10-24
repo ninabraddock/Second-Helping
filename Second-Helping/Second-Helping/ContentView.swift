54b77da899d7cea5de6c21b4f518e6a4cf3ace54
nathanblanchard <njblanch@uvm.edu>
2024-10-24 10:28:56 -0400

    Made login visible again

--- Second-Helping/Second-Helping/ContentView.swift
+++ Second-Helping/Second-Helping/ContentView.swift
@@ -8,22 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isCustomer = true
+//    @State private var isCustomer = true
 //    @State private var isRestaurant = false
     
     @EnvironmentObject var authViewModel: AuthViewModel
     
     var body: some View {
         Group {
-            CustomerView(isLoggedIn: $isCustomer)
-            // Skip over the login process for demonstration purposes
-            if false {
-                if authViewModel.userSession != nil {
-                    ProfileView()
-                } else {
-                    LoginView()
-                    //                LoginView(isCustomer: $isCustomer, isRestaurant: $isRestaurant)
-                }
+            if authViewModel.userSession != nil {
+                ProfileView()
+            } else {
+                LoginView()
+                //                LoginView(isCustomer: $isCustomer, isRestaurant: $isRestaurant)
             }
         }
         
