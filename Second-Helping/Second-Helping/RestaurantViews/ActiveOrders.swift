0e504b4a6fe7f083cf4cf96596f1039dc300ac30
ninabraddock <113557496+ninabraddock@users.noreply.github.com>
2024-10-28 18:57:04 -0400

    created ActiveOrders and PastOrders pages, static, not linked to firebase yet

---
+++ Second-Helping/Second-Helping/RestaurantViews/ActiveOrders.swift
@@ -0,0 +1,18 @@
+//
+//  ActiveOrders.swift
+//  Second-Helping
+//
+//  Created by Nina Braddock on 10/28/24.
+//
+
+import SwiftUI
+
+struct ActiveOrders: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActiveOrders()
+}
---
+++ Second-Helping/Second-Helping/RestaurantViews/PastOrders.swift
@@ -0,0 +1,18 @@
+//
+//  PastOrders.swift
+//  Second-Helping
+//
+//  Created by Nina Braddock on 10/28/24.
+//
+
+import SwiftUI
+
+struct PastOrders: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PastOrders()
+}
