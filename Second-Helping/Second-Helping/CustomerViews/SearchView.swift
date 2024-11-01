2cc6b01d29d0a910c7f0fa50b2f2d5ecc001cf4f
Charlie-Corriero <charlie.corriero@uvm.edu>
2024-11-01 12:43:28 -0400

    Added functions for distance calculations, calculations are correct but the distance is wrong, going to fix soon

--- Second-Helping/Second-Helping/CustomerViews/SearchView.swift
+++ Second-Helping/Second-Helping/CustomerViews/SearchView.swift
@@ -6,9 +6,62 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreLocationUI
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func requestLocation() {
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location: \(error.localizedDescription)")
+    }
+}
+
+func distanceInMiles(userLat: Double, userLong: Double, restLat: Double, restLong: Double) -> Double {
+    let startLocation = CLLocation(latitude: userLat, longitude: userLong)
+    let endLocation = CLLocation(latitude: restLat, longitude: restLong)
+    
+    // Distance in meters
+    let distanceInMeters = startLocation.distance(from: endLocation)
+    
+    // Conversion
+    let distanceInMiles = distanceInMeters / 1609.34
+    let formattedDistance = (distanceInMiles * 10).rounded() / 10
+    
+    return formattedDistance
+}
+
+func distanceTo(userLong: Double?, userLat: Double?, restLat: Double, restLong: Double) -> Double {
+    if let hasUserLong = userLong, let hasUserLat = userLat {
+        return distanceInMiles(userLat: hasUserLat, userLong: hasUserLong, restLat: restLat, restLong: restLong)
+    } else {
+        return 0.0
+    }
+}
 
 struct SearchView: View {
     @EnvironmentObject var restaurantViewModel: RestaurantViewModel
+    @StateObject var locationManager = LocationManager()
+    @State var userLat: Double?
+    @State var userLong: Double?
+    @State var distance: Double?
     
     var body: some View {
         let customGreen = Color(hex: "#4f7942")
@@ -17,6 +70,23 @@ struct SearchView: View {
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(customGreen)
+            
+            if let location = locationManager.location {
+                Text("Successfully found location")
+                    .onAppear {
+                        userLat = location.latitude
+                        userLong = location.longitude
+                    }
+                Text("Longitude: \(String(describing: userLong))")
+                Text("Latitude: \(String(describing: userLat))")
+            }
+            
+            LocationButton {
+                locationManager.requestLocation()
+            }
+            .frame(height: 44)
+            .padding()
+            
             // Section for Dinner
             HStack{
                 Text("Dinner")
@@ -33,6 +103,11 @@ struct SearchView: View {
                 HStack(spacing: 20) {
                     ForEach(restaurantViewModel.restaurants) { restaurant in
                         let dinnerMeals = restaurant.meals.filter { $0.type == "Dinner" }
+                        
+                        var restLat = restaurant.latitude
+                        var restLong = restaurant.longitude
+                        var distanceToRest = distanceTo(userLong: userLong, userLat: userLat, restLat: restLat, restLong: restLong)
+
                         ForEach(dinnerMeals) { meal in
                             ProductCard(
                                 image: Image("waterworks"), // replace with image
@@ -41,7 +116,7 @@ struct SearchView: View {
                                 bagType: meal.bagType,
                                 rangePickUpTime: "\(meal.rangePickUpTime.start) - \(meal.rangePickUpTime.end)",
                                 ranking: restaurant.meanRating,
-                                distance: 0.3, // replace with distance calculation
+                                distance: distanceToRest,
                                 price: meal.price,
                                 btnHandler: nil
                             )
