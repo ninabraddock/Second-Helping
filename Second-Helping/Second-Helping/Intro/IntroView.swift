d3b0caeb1b66499fc558bcbec4635690ee643bc9
nathanblanchard <njblanch@uvm.edu>
2024-11-12 16:27:06 -0500

    Edited loading gif, added intro screen and a new font

---
+++ Second-Helping/Second-Helping/Intro/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  Second-Helping
+//
+//  Created by Nathan Blanchard on 11/12/24.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
